b030b41222a44e6554aff8082c7b8891a069e876
brennen <161400377+brennenxyz@users.noreply.github.com>
2024-05-29 13:01:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const EntityEndermite = Java.type("net.minecraft.entity.monster.EntityEndermite")
+const key = new KeyBind("Enable ThystHider", Keyboard.KEY_NONE, "ThystHider")
+
+let enabled = false;
+
+key.registerKeyPress(() => {
+    enabled = !enabled;
+
+    if(enabled) ChatLib.chat("§4[§cThystHider§4] §5ThystHider §2Enabled!");
+    else ChatLib.chat("§4[§cThystHider§4] §5ThystHider §4Disabled!");
+})
 
 register("renderEntity", (ent, pos, pt, event) => {
-    if(ent instanceof EntityEndermite) cancel(event);
+    if(!enabled) return;
+    if(!ent instanceof EntityEndermite) return;
+
+    cancel(event);
+    ent.func_70106_y()
 })
